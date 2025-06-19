c615d53b945d50041458a3cc66d6820213065b25
inhanbyeol94 <inhanbyeol94@gmail.com>
2025-06-19 17:59:04 +0900

    feat: 권한 데코레이터에 주석 추가
    - 권한 데코레이터의 기능 설명 추가
    - 접근 대상자의 권한 조건에 대한 설명 포함

--- src/common/decorators/role.decorator.ts
+++ src/common/decorators/role.decorator.ts
@@ -1,3 +1,7 @@
 import { Reflector } from '@nestjs/core';
 
+/**
+ * ### 권한 데코레이터
+ * @desc 접근 대상자의 권한이 설정된 권한보다 높거나 같은 경우에만 접근이 허용됩니다.
+ * */
 export const Role = Reflector.createDecorator<number>();
